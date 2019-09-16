1de47b116a6b09095215418aa7e4355313a89364
siddhesh10 <sonusharma.gss@gmail.com>
2019-09-17 04:47:15 +0530

    Adding Indian Fuzzy Name Match Code

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools import setup
+
+def readme():
+    with open("README.md") as f:
+        README = f.read()
+    return README
+
+setup(
+    name="indian-namematch",
+    version="1.0.0",
+    description="Indian Fuzzy name Matching Tool.",
+    long_description = readme(),
+    long_description_content_type = "text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    author="Siddhesh Sharma",
+    author_email="siddheshatjuit@gmail.com",
+    license = 'MIT'    
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=[indian_namematch],
+    include_package_data=True,
+    install_requires = ["re","nltk","permutations"]
+)
